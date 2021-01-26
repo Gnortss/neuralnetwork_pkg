19243b605d4b3ad802f827d81d5c8f739c1b5e6d
Jure Casar <jure.casar1@gmail.com>
2021-01-26 13:49:51 +0100

    added backpropagation for supervised learning

--- neuralnetwork_pkg/main.py
+++ neuralnetwork_pkg/main.py
@@ -3,3 +3,9 @@ from neuralnetwork import *
 
 nn = NeuralNetwork(2, [3, 4, 3], 2)
 # print(nn.feedforward(np.array([1,1])))
+
+# print('layer outputs')
+# for layer in nn.layers:
+#   print(layer.outputs, layer.act_d(layer.outputs))
+
+nn.train(np.array([1, 1]), np.array([0, 1]))
--- neuralnetwork_pkg/neuralnetwork.py
+++ neuralnetwork_pkg/neuralnetwork.py
@@ -7,6 +7,12 @@ class Layer:
     self.w = np.random.randn(nodes, input_nodes)
     self.b = np.zeros(nodes)
     self.activation=activation
+    self.act_f = lambda x: 1/(1 + np.exp(-x))
+    self.act_d = lambda x: x*(1-x) # Expected x is output which is already a sigmoid
+
+  def calc_outputs(self, input):
+    self.outputs = self.act_f((self.w @ input) + self.b)
+    return self.outputs
 
 class NeuralNetwork:
   def __init__(self, input_nodes, hidden_layer_format, output_nodes):
@@ -17,6 +23,7 @@ class NeuralNetwork:
     self.layers = []
     self.build_layers()
 
+    self.learning_rate = 0.1
 
   def build_layers(self):
     layers = [self.input_nodes] + self.hidden_layer_format + [self.output_nodes]
@@ -24,11 +31,23 @@ class NeuralNetwork:
       self.layers.append(Layer(input_nodes, hidden_nodes))
 
   def feedforward(self, a):
-    def calc_layer(layer_index, a):
-      def sigmoid(x):
-        return 1/(1 + np.exp(-x))
-      if layer_index >= len(self.layers):
-        return a
-      return calc_layer(layer_index+1, sigmoid((self.layers[layer_index].w @ a) + self.layers[layer_index].b))
+    outputs = self.layers[0].calc_outputs(a)
+    for layer in self.layers[1:]:
+      outputs = layer.calc_outputs(outputs)
+    return outputs
 
-    return calc_layer(0, a)
+  def train(self, inputs, targets):
+    def backpropagate(layer_index, errors):
+      l = self.layers[layer_index]
+      l_1 = self.layers[layer_index-1]
+      gradient = l.act_d(l.outputs) * errors * self.learning_rate
+      delta_w = gradient.reshape(len(gradient), 1) @ np.atleast_2d(l_1.outputs)
+      l.w = l.w + delta_w
+      if layer_index == 1:
+        return
+      n_errors = np.transpose(l.w) @ errors
+      backpropagate(layer_index-1, n_errors)
+
+    outputs = self.feedforward(inputs)
+    output_errors = targets-outputs
+    backpropagate(len(self.layers)-1, output_errors)
