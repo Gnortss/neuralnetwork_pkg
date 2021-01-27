7384339d0b0a6d6010abadd135b815656dda370e
Jure Casar <jure.casar1@gmail.com>
2021-01-27 11:16:22 +0100

    minor changes

--- neuralnetwork_pkg/main.py
+++ neuralnetwork_pkg/main.py
@@ -1,10 +1,28 @@
 import numpy as np
 from neuralnetwork import *
 import random
+# import tensorflow as tf
 
-training_data = [((0,0), (0)), ((0,1), (1)), ((1,0), (1)), ((1,1), (0))]
+# mnist = tf.keras.datasets.mnist
 
-nn = NeuralNetwork(2, [2], 1)
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# x_train = tf.keras.utils.normalize(x_train, axis=1).reshape(len(x_train), 784)
+# x_test = tf.keras.utils.normalize(x_test, axis=1).reshape(len(x_test), 784)
+
+
+# nn = NeuralNetwork(784)
+# nn.add(Layer(784, 128, activation='relu'))
+# nn.add(Layer(128, 128, activation='relu'))
+# nn.add(Layer(128, 10, activation='softmax'))
+
+training_data = [((0,0), 0), ((0,1), 1), ((1,0), 1), ((1,1), 0)]
+
+# nn = NeuralNetwork(2, [2], 1)
+nn = NeuralNetwork(2)
+nn.add(Layer(2, 2))
+nn.add(Layer(2, 2))
+nn.add(Layer(2, 1))
 
 for _ in range(100000):
   inputs, targets = random.choice(training_data)
--- neuralnetwork_pkg/neuralnetwork.py
+++ neuralnetwork_pkg/neuralnetwork.py
@@ -3,19 +3,38 @@ import itertools
 
 class Layer:
   """A class to represent a layer in NeuralNetwork"""
-  def __init__(self, input_nodes, nodes, activation='sigmoid'):
+  def __init__(self, input_nodes : int, nodes : int, activation='sigmoid'):
     self.w = np.random.randn(nodes, input_nodes)
     self.b = np.zeros(nodes)
     self.activation=activation
-    self.act_f = lambda x: 1/(1 + np.exp(-x))
-    self.act_d = lambda x: x*(1-x) # Expected x is output which is already a sigmoid
+    self.setup_activation(activation)
 
   def calc_outputs(self, input):
-    self.outputs = self.act_f((self.w @ input) + self.b)
+    self.z = (self.w @ input) + self.b
+    self.outputs = self.act_f(self.z)
     return self.outputs
 
+  def setup_activation(self, activation):
+    if activation == 'sigmoid':
+      self.act_f = lambda x: 1/(1 + np.exp(-x))
+      self.act_d = lambda x: self.act_f(x)*(1-self.act_f(x)) 
+    elif activation == 'relu':
+      self.act_f = lambda x: np.maximum(0, x)
+      self.act_d = lambda x: 1*(x > 0)
+    # elif activation == 'softmax':
+    #   def softmax(x):
+    #     exps = np.exp(x - x.max())
+    #     return exps / np.sum(exps)
+    #   def softmax_derivative(x):
+    #     x = softmax(x)
+    #     s = x.reshape(-1,1)
+    #     return np.diagflat(s) - np.dot(s, s.T)
+    #   self.act_f = softmax
+    #   self.act_d = softmax_derivative
+      
+
 class NeuralNetwork:
-  def __init__(self, input_nodes, hidden_layer_format, output_nodes):
+  def __init__(self, input_nodes : int, hidden_layer_format : list, output_nodes : int):
     self.input_nodes = input_nodes
     self.hidden_layer_format = hidden_layer_format
     self.output_nodes = output_nodes
@@ -24,6 +43,15 @@ class NeuralNetwork:
     self.build_layers()
 
     self.learning_rate = 0.1
+
+  def __init__(self, input_nodes):
+    self.input_nodes = input_nodes
+    self.learning_rate = 0.1
+    self.layers = []
+
+  def add(self, layer : Layer):
+    self.output_nodes = len(layer.b)
+    self.layers.append(layer)
 
   def build_layers(self):
     layers = [self.input_nodes] + self.hidden_layer_format + [self.output_nodes]
@@ -40,11 +68,13 @@ class NeuralNetwork:
     def backpropagate(layer_index, errors):
       l = self.layers[layer_index]
       l_1 = self.layers[layer_index-1]
-      gradients = l.act_d(l.outputs) * errors * self.learning_rate
+
+      derivative = l.act_d(l.z)
+      gradients = derivative * errors * self.learning_rate
       if layer_index > 0:
-        delta_w = gradients.reshape(len(gradients), 1) @ np.atleast_2d(l_1.outputs)
+        delta_w = gradients.reshape(-1, 1) @ np.atleast_2d(l_1.outputs)
       else:
-        delta_w =  gradients.reshape(len(gradients),1 ) @ np.atleast_2d(inputs)
+        delta_w =  gradients.reshape(-1,1 ) @ np.atleast_2d(inputs)
       # Adjust weights and biases by their deltas
       l.w = l.w + delta_w
       l.b = l.b + gradients
